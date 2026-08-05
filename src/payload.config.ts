6534ad56a012d74165675d4624de2a905979e099
Abolfazl Mohsenpour <mohsenpour@art-tempi.de>
2026-08-05 16:07:47 +0330

    first deploy attempt

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -55,5 +56,23 @@ export default buildConfig({
     },
   }),
   sharp,
-  plugins: [],
+  plugins: [
+    ...(process.env.S3_BUCKET
+      ? [
+          s3Storage({
+            collections: { media: true },
+            bucket: process.env.S3_BUCKET,
+            config: {
+              credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+              },
+              endpoint: process.env.S3_ENDPOINT || '',
+              region: process.env.S3_REGION || 'us-east-1',
+              forcePathStyle: true,
+            },
+          }),
+        ]
+      : []),
+  ],
 })
